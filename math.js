c31b789f92729fb87ae9b2f6d719d7665da0ac72
john-galvao <galvao_22@yahoo.com>
2026-01-15 20:26:17 -0800

    adding new folders from other repository

---
+++ math.js
@@ -0,0 +1,23 @@
+function double(n) {
+  return n * 2;
+}
+
+function square(n) {
+  return n * n;
+}
+
+function isEven(n) {
+  return n % 2 === 0;
+}
+
+function isOdd(n) {
+  return n % 2 !== 0;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function half(n) {
+  return n / 2;
+}
